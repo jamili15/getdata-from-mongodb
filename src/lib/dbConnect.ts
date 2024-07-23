437c3c8479c8f0e5ff5ed25cbea2b62e02ad5e41
James Kevin Jamili <jameskevin.ramesessystems@gmail.com>
2024-07-23 08:44:46 +0800

    updated

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
 
-const connection: { isConnected?: number } = {};
-
-async function dbConnect() {
-  if (connection.isConnected) {
-    return;
+const dbConnect = async () => {
+  if (mongoose.connections[0].readyState) {
+    return true;
   }
 
-  const db = await mongoose.connect(process.env.MONGODB_URI!);
-
-  connection.isConnected = db.connections[0].readyState;
-}
+  try {
+    await mongoose.connect(process.env.MONGODB_URI!);
+    console.log("Mongodb Connected");
+    return true;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export default dbConnect;
